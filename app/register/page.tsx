11c6b049909e8674066c5302ae1478787cf6c36d
bugsm <121507446+bugsm@users.noreply.github.com>
2025-04-23 19:57:59 +0700

    test

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -46,7 +46,7 @@ export default function RegisterPage() {
       const userData = {
         firstName,
         lastName,
-        email,
+        email ,
         password,
         newsletter,
       }
